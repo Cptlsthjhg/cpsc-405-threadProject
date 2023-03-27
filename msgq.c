ed9194d827abc307bb2b034aca2f2c0ef1dca7f5
Ethan Thompson <ethan.m.thompson@gmail.com>
2023-03-27 22:22:25 +0000

    added our working files.

---
+++ msgq.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+struct msgq *msgq_init(int num_msgs){}
+
+int msgq_send(struct msgq *mq, char *msg){}
+
+char *msgq_recv(struct msgq *mq){}
+
+int msgq_len(struct msgq *mq){}
+
+void msgq_show(struct msgq *mq){}
+
